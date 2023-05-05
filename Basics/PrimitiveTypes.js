4b7c93d2b77e760e0b2fcab18914b3828c1056f9
gabriela soares <97314635+GabyySoares@users.noreply.github.com>
2023-05-05 15:41:43 -0300

    variáveis com difererentes tipos de dados
    inicialização de cinco variáveis diferentes usando a palavra-chave "let". Cada variável contém um tipo diferente de dado, como uma string, número, booleano, undefined ou null.

---
+++ Basics/PrimitiveTypes.js
@@ -0,0 +1,15 @@
+// Definindo cinco variáveis usando "let" e atribui valores a elas
+
+let name = 'Mosh';
+let age = 30; 
+let isApproved = false; 
+let firsName = undefined; // "undefined" é um valor primitivo que indica que a variável não foi inicializada.
+let lasName = null; // "null" é um valor primitivo que representa a ausência intencional de qualquer objeto ou valor.
+let selectedColar = null; // A declaração "let selectedColor = null" é uma convenção comum para indicar que a variável está vazia ou não tem um valor selecionado no momento.
+
+// as variáveis são usadas para armazenar dados e valores.
+// O tipo de dado armazenado pode ser uma string, número, booleano, undefined ou nulo.
+// nome das variáveis é sensível a maiúsculas e minúsculas.
+// As variáveis podem ser declaradas usando "var", "let" ou "const", e cada uma tem suas próprias regras de escopo.
+
+
