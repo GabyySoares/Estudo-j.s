91027d92ac0f6c730dbed2bc25378f47f876c523
gabriela soares <97314635+GabyySoares@users.noreply.github.com>
2023-05-05 15:57:27 -0300

    Objetos em J.s
    Como adicionar e atualizar propriedades em objetos JavaScript usando notação de ponto e colchetes. O código define um objeto "person" com duas propriedades - "name" e "age". Em seguida, o valor da propriedade "name" é alterado de "Mosh" para "John" usando notação de ponto. Depois, uma nova propriedade "selection" é adicionada ao objeto usando notação de colchetes e seu valor é definido como "Mary". Por fim, o valor da propriedade "name" é impresso no console usando notação de ponto. O commit tem como objetivo ensinar sobre a manipulação de objetos em JavaScript e a diferença entre notação de ponto e colchetes para acessar e atualizar propriedades de objetos.

---
+++ Basics/Objects.js
@@ -0,0 +1,16 @@
+// Define três variáveis usando "let" e atribui valores a elas
+
+let name = 'Mosh';
+let age = 30;
+let person = {
+     name: 'Mosh',
+     age: 30
+};
+// Altera o valor da propriedade "name" do objeto "person"
+person.name = 'John';
+
+// Define uma variável "selection" e usa a notação de colchetes para adicionar uma nova propriedade "selection" ao objeto "person"
+let selection = 'name';
+person['selection'] = 'Mary';
+
+console.log(person.name);
