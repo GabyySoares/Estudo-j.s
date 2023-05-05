51665cc7b3e2a27ad67f0be5dd40f33fd714d0b6
gabriela soares <97314635+GabyySoares@users.noreply.github.com>
2023-05-05 15:23:48 -0300

    Declarando variáveis em JS
    Inicializando  variáveis e imprime resultados no console. Aprendendo sobre variáveis em JavaScript e as diferenças entre 'var' e 'let'

---
+++ Basics/index.js
@@ -0,0 +1,11 @@
+// Define a variável "name" e atribui o valor "mosh"
+var name = 'mosh'; 
+// Imprime o valor da variável "name" no console
+console.log(name);
+
+
+// Define duas variáveis, "firstName" e "lastName", usando "let"
+let firstName = 'Mosth';
+let  lastName = 'Hamedani';
+// Em JavaScript, as variáveis podem ser definidas com "var" ou "let".
+//A diferença entre "let" e "var" é que "let" é limitado a um bloco de código, enquanto "var" é uma variável global
