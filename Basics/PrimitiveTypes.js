bc6eb590eaabe48cf66161b7f27be1514442d013
gabriela soares <97314635+GabyySoares@users.noreply.github.com>
2023-05-05 16:00:47 -0300

    Corrigindo

--- Basics/PrimitiveTypes.js
+++ Basics/PrimitiveTypes.js
@@ -5,7 +5,7 @@ let age = 30;
 let isApproved = false; 
 let firsName = undefined; // "undefined" é um valor primitivo que indica que a variável não foi inicializada.
 let lasName = null; // "null" é um valor primitivo que representa a ausência intencional de qualquer objeto ou valor.
-let selectedColar = null; // A declaração "let selectedColor = null" é uma convenção comum para indicar que a variável está vazia ou não tem um valor selecionado no momento.
+let selectedColosr = null; // A declaração "let selectedColor = null" é uma convenção comum para indicar que a variável está vazia ou não tem um valor selecionado no momento.
 
 // as variáveis são usadas para armazenar dados e valores.
 // O tipo de dado armazenado pode ser uma string, número, booleano, undefined ou nulo.
