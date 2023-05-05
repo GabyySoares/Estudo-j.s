d3496c7642b9f05ca3f2045d0737364419e5624e
gabriela soares <97314635+GabyySoares@users.noreply.github.com>
2023-05-05 16:08:39 -0300

    Adicionando um novo elemento a um array
    adicionando um novo elemento a um array em JavaScript, mostrando como arrays em JavaScript são dinâmicos e podem ter elementos de diferentes tipos em posições distintas. O código cria um array de duas strings, adiciona um terceiro elemento ao array como um número e, em seguida, imprime o comprimento do array no console. Isso é útil para entender como manipular arrays em JavaScript e como adicionar elementos a um array existente.

--- Basics/Arrays.js
+++ Basics/Arrays.js
@@ -1,1 +1,7 @@
-let selectedColos
+
+ // Cria um array chamado "selectedColors" com dois elementos
+let selectedColors = ['red', 'blue'];  
+// Adiciona um terceiro elemento ao array, definido como o número 1
+selectedColors[2] = 1 ;
+console.log (selectedColors.length);
+
