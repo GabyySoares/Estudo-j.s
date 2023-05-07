1871271b3fe073a1e54e70ab17af733cfeecb3ab
gabriela soares <97314635+GabyySoares@users.noreply.github.com>
2023-05-07 20:46:34 -0300

    operações matemáticas básicas em JavaScript
    Realizando operações matemáticas básicas com variáveis e imprimindo os resultados no console, além de demonstrar o uso dos operadores de incremento e decremento em JavaScript.

---
+++ Operators/aritméticos.js
@@ -0,0 +1,21 @@
+ // Atribuindo valores às variáveis x e y
+let x = 10;
+let y = 3;
+
+
+// Operações matemáticas entre as variáveis e impressão no console
+console.log(x+y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+console.log( x ** y);
+
+
+// Incrementando o valor de x em 1 e depois imprimindo o valor de x
+console.log(x++);
+console.log(x);
+
+// Decrementando o valor de x em 1 e depois imprimindo o valor de x
+console.log(--x);
+
