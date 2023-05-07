314abcbca92f780fa28eedf88c1f3bb9a81694b3
gabriela soares <97314635+GabyySoares@users.noreply.github.com>
2023-05-07 20:57:05 -0300

    Operador de atribuição
    Adiciona um código em JavaScript que realiza operações matemáticas utilizando atribuição abreviada, mostrando como é possível simplificar a escrita do código sem alterar sua lógica

---
+++ Operators/Assignment.js
@@ -0,0 +1,9 @@
+ // Declara a variável x e atribui o valor 10 a ela
+let x = 10;
+
+
+x = x + 5; // Adiciona 5 a x, resultando em 15
+x += 5; // Abreviação de x = x + 5, adiciona 5 a x novamente, resultando em 20
+
+x = x * 3; // Multiplica x por 3, resultando em 60
+x *=3; // Abreviação de x = x * 3, multiplica x por 3 novamente, resultando em 180
