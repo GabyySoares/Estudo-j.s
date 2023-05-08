eb0dc799c6a6d66d8410cad5a9fc2590b569d40d
gabriela soares <97314635+GabyySoares@users.noreply.github.com>
2023-05-07 21:04:01 -0300

    Adicionando operadores relacionais e de igualdade
    O código contém exemplos de uso dos operadores ">" (maior que), ">=" (maior ou igual a), "<" (menor que), "<=" (menor ou igual a), "==" (igual a) e "!==" (diferente de).

---
+++ Operators/Comparison.js
@@ -0,0 +1,12 @@
+let x = 1;
+
+//Operadores Relacionais
+console.log( x > 0); // true, pois x é maior que 0
+console.log(x >=1); // true, pois x é maior que
+console.log( x < 1);  // false, pois x não é menor que 1
+console.log( x <= 1 ); // true, pois x é menor ou igual a 
+
+
+//Operadores de Igualdade
+console.log(x==1);
+console.log(x !==1);
