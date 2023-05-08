9d0d42ff096e8bfe774cc259eb01b69a218cb75a
gabriela soares <97314635+GabyySoares@users.noreply.github.com>
2023-05-07 23:34:58 -0300

    loops para imprimir propriedades de um objeto e elementos de um array
    Adicionando dois loops diferentes - um para iterar sobre as propriedades de um objeto e outro para iterar sobre os elementos de um array - e imprime no console tanto o nome da propriedade / índice quanto o valor correspondente em cada iteração.

---
+++ ControlFlow/For-in.js
@@ -0,0 +1,16 @@
+// Declaração de um objeto com duas propriedades: name e age
+const person= {
+     name: 'Mosh',
+     age:30
+};
+
+// Loop for..in que itera sobre as propriedades do objeto person
+for ( let key in person)
+  console.log(key , person[key]); // Imprime o nome e valor de cada propriedade
+
+  // Declaração de um array com três elementos
+const colors = ['red', 'green', 'blue'];
+
+// Loop for..in que itera sobre os índices do array colors
+for (let index in colors)
+   console.log(index, colors[index]); // Imprime o índice e valor de cada elemento
