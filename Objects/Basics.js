525dc467d8faa8a62c5a997ed4111be52a50e824
gabriela soares <97314635+GabyySoares@users.noreply.github.com>
2023-05-08 07:09:14 -0300

    Criação de objeto usando uma função construtora
    : Este commit adiciona o código que cria um objeto 'myCircle' usando uma função construtora e atribui a variável 'circle1'. Também adiciona um objeto vazio atribuído à variável 'x'.

--- Objects/Basics.js
+++ Objects/Basics.js
@@ -33,3 +33,20 @@ console.log(circle1);
 // Cria um objeto circle2 com raio igual a 2 usando a Factory Function createCircle
 const circle2 = createCircle(2);
 console.log(circle2);
+
+// Constructor Function
+
+// Define uma função construtora chamada 'Circle' que recebe um parâmetro 'radius
+function Circle(radius){
+     // Define uma propriedade 'radius' no objeto 'this' que representa a instância da função construtora
+    this.radius = radius;
+     // Define um método 'draw' no objeto 'this' que representa a instância da função construtora
+    this.draw = function(){
+         console.log('draw');
+    }
+}
+// Cria um método 'draw' no objeto 'this'
+const myCircle = new Circle(1);
+// Cria um objeto vazio chamado 'x'
+const x = {};
+
