c4239007e1daf803c7a31f402dc72febcfd913b4
gabriela soares <97314635+GabyySoares@users.noreply.github.com>
2023-05-07 23:04:35 -0300

    funcionalidade de impressão de números ímpares
    Implementando  duas novas funções para imprimir apenas os números ímpares em um intervalo de 0 a 5. Uma função usa um loop for e a outra usa um loop while

---
+++ ControlFlow/While.js
@@ -0,0 +1,11 @@
+ // Loop for
+for (let i = 0; i <= 5; i++){
+    if (i % 2 !==0) console.log(i); // Imprime na tela apenas os números ímpares
+}
+
+// Loop while
+let i = 0;
+while (i <= 5){
+    if ( i % 2 !==0) console.log(i);
+    i++; // Imprime na tela apenas os números ímpares
+}
