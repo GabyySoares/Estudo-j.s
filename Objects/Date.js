7ca5136f6f753418eafe4e62549896ee599eb933
gabriela soares <97314635+GabyySoares@users.noreply.github.com>
2023-05-08 07:56:20 -0300

    Atualizando o ano da data atual
    Modificou o ano da data atual usando o método setFullYear() do objeto Date

---
+++ Objects/Date.js
@@ -0,0 +1,8 @@
+// Cria um objeto Date com a data e hora atuais
+const now = new Date ();
+// Cria um objeto Date com a data e hora "May 11 2018 09:00"
+const date1 = new Date ('May 11 2018 09:00');
+// Cria um objeto Date com a data e hora "2018-05-11T09:00:00.000Z"
+const date2 = new Date(2018, 4 , 11 , 9);
+
+now.setFullYear(2017); // Define o ano do objeto "now" para 2017
