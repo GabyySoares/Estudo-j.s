38553412c08736e115f385062a05c93e301a4d8f
gabriela soares <97314635+GabyySoares@users.noreply.github.com>
2023-05-07 23:15:26 -0300

    Adicionando um novo loop

--- ControlFlow/While.js
+++ ControlFlow/While.js
@@ -1,11 +1,18 @@
  // Loop for
-for (let i = 0; i <= 5; i++){
-    if (i % 2 !==0) console.log(i); // Imprime na tela apenas os números ímpares
-}
+//for (let i = 0; i <= 5; i++){
+   // if (i % 2 !==0) console.log(i); // Imprime na tela apenas os números ímpares
+//}
 
 // Loop while
-let i = 0;
-while (i <= 5){
+//let i = 0;
+//while (i <= 5){
+    //if ( i % 2 !==0) console.log(i);
+   // i++; // Imprime na tela apenas os números ímpares
+//}
+
+// do-while 
+let i = 9;
+do{
     if ( i % 2 !==0) console.log(i);
-    i++; // Imprime na tela apenas os números ímpares
-}
+    i++;
+} while(i <=5);
