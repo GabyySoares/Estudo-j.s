c59e4bc638d5b8efd3549569652e90e0f2597cbc
gabriela soares <97314635+GabyySoares@users.noreply.github.com>
2023-05-07 22:26:46 -0300

    utilizando o for para criar um loop
    O for percorre os valores de i de 5 a 1, e imprime no console apenas os valores ímpares (5, 3, 1)

---
+++ ControlFlow/For.js
@@ -0,0 +1,8 @@
+// Declaração de um loop for que começa com i = 5, testa se i >= 1, e decrementa i em 1 a cada iteração
+
+for (let i = 5; i >= 1; i--){ 
+     // Verifica se o valor de i é ímpar (ou seja, se o resto da divisão de i por 2 é diferente de zero)
+     if (i % 2 !==0)
+     // Se a condição for verdadeira, o valor de i é impresso no console
+      console.log(i);
+}
