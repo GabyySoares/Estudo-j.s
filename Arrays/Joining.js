672ee89b6e182d160ccf2db32b9d36c8ca00a1ac
gabriela soares <97314635+GabyySoares@users.noreply.github.com>
2023-05-08 14:40:11 -0300

    exemplos de uso dos métodos join() e split() em arrays
     exemplos de como usar os métodos join() e split() em JavaScript para converter entre arrays e strings. O método join() é usado para juntar os elementos de um array em uma única string, enquanto o método split() é usado para dividir uma string em um array de caracteres individuais. Foram utilizados diferentes tipos de dados, como números e strings, para ilustrar o uso desses métodos

---
+++ Arrays/Joining.js
@@ -0,0 +1,10 @@
+const number = [1,2,3];
+const joint = numbers.join(','); // junta os elementos do array em uma única string, separando-os por vírgula
+console.log(joined);
+
+const message =  'This is my first message';  // declara uma constante "message" contendo uma string
+const parts = message.split(''); // divide a string em um array de caracteres individuais
+console.log(parts);
+
+const combined = parts.join('-');  // junta os caracteres do array em uma única string, separando-os por hífen
+console.log(combined);
