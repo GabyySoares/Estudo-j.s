3f4aac0a52c40846e234935ccb304f681a5397bc
gabriela soares <97314635+GabyySoares@users.noreply.github.com>
2023-05-08 16:00:58 -0300

    Declaração de variáveis e função inicial
     declaração das variáveis 'color' e 'age' e uma função chamada 'sayHi'. A variável 'color' é inicializada com um valor de 'red', enquanto a variável 'age' é inicializada com um valor de 30 usando a palavra-chave 'let'. A função 'sayHi' é uma função básica que registra a mensagem 'hi' no console

---
+++ Functions/Let-Var.js
@@ -0,0 +1,7 @@
+var color = 'red'; // Declara uma variável 'color' e atribui o valor 'red'
+let age = 30; // Declara uma variável 'age' usando o let e atribui o valor 30
+
+// Declara uma função chamada 'sayHi' que imprime 'hi' no console
+function sayHi(){
+    console.log('hi');
+}
