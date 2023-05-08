2f8c755014c12c7cdd7d6e8cdda90083c7e06d96
gabriela soares <97314635+GabyySoares@users.noreply.github.com>
2023-05-08 07:49:40 -0300

    Update String.js

--- Objects/String.js
+++ Objects/String.js
@@ -4,3 +4,6 @@ const message = 'This is my\n first message'; // Declara uma variável 'message'
 
 // String object
 const another = new String ('hi'); // Declara uma variável 'another' com uma instância de objeto String contendo a string 'hi'
+
+
+
