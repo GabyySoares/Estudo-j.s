d3f8c8cea3b9adb715360fa4ab0dc65883f76cb5
gabriela soares <97314635+GabyySoares@users.noreply.github.com>
2023-05-07 23:22:26 -0300

    Adicionando loop infinito

---
+++ ControlFlow/Infinite-loops.js
@@ -0,0 +1,18 @@
+let i = 0;
+while (i < 5) {
+     console.log(i);
+     i++;
+}
+
+while (true) {
+   // Código infinito sem instrução de saída
+}
+
+let x = 0;
+do {
+    //x++;
+} while (x < 5);
+
+for (let i = 0; i < 10; i++) {
+   // Bloco de código do loop for
+}
