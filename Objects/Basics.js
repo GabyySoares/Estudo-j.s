52e5b63c271170594f44986353090ca2e42cebab
gabriela soares <97314635+GabyySoares@users.noreply.github.com>
2023-05-08 06:52:45 -0300

    Adicionado exemplo de função factory
    Adicionado exemplo de código de uma função factory para criar objetos de círculo com um raio fornecido e um método de desenho. Demonstrado a criação de dois objetos de círculo usando a função factory.

--- Objects/Basics.js
+++ Objects/Basics.js
@@ -13,3 +13,23 @@ const circle = {
 
 // Chama o método 'draw' do objeto 'circle'
 circle.draw();  // imprime 'draw' no console
+
+// Factory Function
+
+// Factory Function que retorna um objeto circle com uma propriedade de raio e um método draw
+function createCircle(radius){
+    return{
+        radius,
+        draw(){
+            console.log('draw');
+        }
+    };
+}
+
+// Cria um objeto circle1 com raio igual a 1 usando a Factory Function createCircle
+const circle1 = createCircle(1);
+console.log(circle1);
+
+// Cria um objeto circle2 com raio igual a 2 usando a Factory Function createCircle
+const circle2 = createCircle(2);
+console.log(circle2);
