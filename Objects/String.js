d918ec592bde0b10f0b67f035fd4f55f6cc66a47
gabriela soares <97314635+GabyySoares@users.noreply.github.com>
2023-05-08 07:39:20 -0300

    exemplos de uso de strings primitivas e objetos
    O código apresenta exemplos de uso de strings primitivas e objetos em JavaScript, mostrando a diferença entre a declaração de uma string primitiva e de um objeto String.

---
+++ Objects/String.js
@@ -0,0 +1,6 @@
+// String primitive
+const message = 'This is my\n first message'; // Declara uma variável 'message' com uma string primitiva contendo uma quebra de linha
+
+
+// String object
+const another = new String ('hi'); // Declara uma variável 'another' com uma instância de objeto String contendo a string 'hi'
