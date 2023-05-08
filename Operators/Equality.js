9b9a2367ff081c0e7ea6b74dce70cb5afbb84cb4
gabriela soares <97314635+GabyySoares@users.noreply.github.com>
2023-05-07 21:15:06 -0300

    Adicionado  comparações com operador de igualdade
    fazendo três comparações utilizando o operador de igualdade (==). A primeira comparação é entre dois números, a segunda é entre uma string contendo um número e um número, e a terceira é entre um valor booleano (true) e um número.

---
+++ Operators/Equality.js
@@ -0,0 +1,12 @@
+
+console.log(1 ==1);
+console.log('1'== 1);
+
+// Comparação de valores numéricos
+console.log(1 ==1);// true
+
+// Comparação de valores numéricos e string
+console.log('1' == 1);// true
+
+// Comparação de valores booleanos e numéricos
+console.log(true ==1 ); //true 
