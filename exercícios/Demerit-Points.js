9e23e4a8b0a10ced1d385d79574c8370c9fd279c
gabriela soares <97314635+GabyySoares@users.noreply.github.com>
2023-05-17 10:18:10 -0300

    implementação da função checkSpeed
    A função checkSpeed recebe um parâmetro chamado speed, que representa a velocidade do veículo.

---
+++ exercícios/Demerit-Points.js
@@ -0,0 +1,17 @@
+chechkSpeed(130);
+
+ function chechkSpeed(speed){
+    const speedLimit = 70;
+    const kmPerPoint = 5;
+
+    if (speed < speedLimit + kmPerPoint);
+      console.log('ok');
+      return;
+ }
+ const points = Math.floor((speed - speedLimit) / kmPerPoint);{
+   if (points >= 12)
+     console.log('License suspended');
+ else
+   console.log('Points', points);
+ }
+
